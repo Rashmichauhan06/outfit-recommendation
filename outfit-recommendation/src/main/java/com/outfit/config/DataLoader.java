475e3daffa9a971d9f059b04ac82f35366b3f0ee
Rashmichauhan06 <132540819+Rashmichauhan06@users.noreply.github.com>
2026-02-03 16:05:59 +0530

    Update DataLoader.java

--- outfit-recommendation/src/main/java/com/outfit/config/DataLoader.java
+++ outfit-recommendation/src/main/java/com/outfit/config/DataLoader.java
@@ -14,7 +14,7 @@ public class DataLoader {
     @Bean
     CommandLineRunner loadData(ProductRepository repository) {
         return args -> {
-            // If you want fresh seed every time, comment this out.
+           
             if (repository.count() > 0) return;
 
             List<Product> products = List.of(
